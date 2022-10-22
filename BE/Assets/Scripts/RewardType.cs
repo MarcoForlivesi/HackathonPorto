cea6bb224c170940dcad3c96abefcf28c9dfe498
m.forlivesi <m.forlivesi@reply.it>
2022-10-22 01:11:48 +0100

    BE project

---
+++ BE/Assets/Scripts/RewardType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardType : MonoBehaviour
+{
+    [SerializeField] private RewardEnum rewardEnum;
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text description;
+}
